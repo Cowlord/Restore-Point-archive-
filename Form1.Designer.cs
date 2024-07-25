59948f8779bf81cc725fc7c7c4ae835c3983c908
Cowlord <Brett@Cowlord.com>
2024-07-25 09:20:25 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Restore_Point
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RestorePoint_Text = new System.Windows.Forms.TextBox();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // RestorePoint_Text
+            // 
+            this.RestorePoint_Text.Location = new System.Drawing.Point(12, 12);
+            this.RestorePoint_Text.Name = "RestorePoint_Text";
+            this.RestorePoint_Text.Size = new System.Drawing.Size(232, 20);
+            this.RestorePoint_Text.TabIndex = 0;
+            this.RestorePoint_Text.Text = "Enter Restore Point Name...";
+            this.RestorePoint_Text.TextChanged += new System.EventHandler(this.RestorePoint_Text_TextChanged);
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Location = new System.Drawing.Point(250, 12);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(75, 23);
+            this.Save_Button.TabIndex = 1;
+            this.Save_Button.Text = "Save";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            this.Save_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(311, 160);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(336, 38);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.RestorePoint_Text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restore Point";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox RestorePoint_Text;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restore_Point
+{
+    public partial class Form1 : Form
+    {
+        private const string PlaceholderText = "Enter Restore Point Name...";
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestorePoint_Text.KeyDown += new KeyEventHandler(RestorePoint_Text_KeyDown);
+            RestorePoint_Text.KeyPress += new KeyPressEventHandler(RestorePoint_Text_KeyPress);
+            RestorePoint_Text.Leave += new EventHandler(RestorePoint_Text_Leave);
+            RestorePoint_Text.Text = PlaceholderText;
+            RestorePoint_Text.ForeColor = Color.Gray;
+
+            // Load restore points when the form loads
+            this.Load += new EventHandler(Form1_Load);
+
+            // Handle delete key press for listBox1
+            listBox1.KeyDown += new KeyEventHandler(listBox1_KeyDown);
+        }
+
+        private void Save_Button_Click(object sender, EventArgs e)
+        {
+            string restorePointName = RestorePoint_Text.Text;
+            if (restorePointName == PlaceholderText)
+            {
+                MessageBox.Show("Please enter a valid restore point name.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string command = $"/c wmic.exe /Namespace:\\\\root\\default Path SystemRestore Call CreateRestorePoint \"{restorePointName}\", 100, 7";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "cmd.exe";
+            psi.Arguments = command;
+            psi.WindowStyle = ProcessWindowStyle.Minimized;
+            psi.Verb = "runas"; // Request elevation
+
+            try
+            {
+                // Minimize the form before starting the process
+                this.WindowState = FormWindowState.Minimized;
+
+                Process process = Process.Start(psi);
+                process.WaitForExit();
+
+                if (process.ExitCode == 0)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to create restore point.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Win32Exception ex)
+            {
+                MessageBox.Show($"Failed to run cmd: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RestorePoint_Text_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // Prevent the beep sound
+                Save_Button_Click(sender, e);
+            }
+        }
+
+        private void RestorePoint_Text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (RestorePoint_Text.Text == PlaceholderText)
+            {
+                RestorePoint_Text.Text = "";
+                RestorePoint_Text.ForeColor = Color.Black;
+            }
+        }
+
+        private void RestorePoint_Text_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(RestorePoint_Text.Text))
+            {
+                RestorePoint_Text.Text = PlaceholderText;
+                RestorePoint_Text.ForeColor = Color.Gray;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //LoadRestorePoints();
+        }
+
+        private void LoadRestorePoints()
+        {
+            try
+            {
+                ManagementScope scope = new ManagementScope("\\\\.\\root\\default");
+                ObjectQuery query = new ObjectQuery("SELECT * FROM SystemRestore");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+                ManagementObjectCollection queryCollection = searcher.Get();
+
+                listBox1.Items.Clear();
+                foreach (ManagementObject m in queryCollection)
+                {
+                    string description = m["Description"].ToString();
+                    DateTime creationTime = ManagementDateTimeConverter.ToDateTime(m["CreationTime"].ToString());
+                    string restorePointInfo = $"{description,-30} {creationTime.ToString("MM/dd/yy @ HH:mm"),30}";
+                    listBox1.Items.Add(restorePointInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load restore points: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && listBox1.SelectedItem != null)
+            {
+                DialogResult result = MessageBox.Show("This will delete all restore points. Do you want to proceed?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        // Disable system protection
+                        ProcessStartInfo disableProtection = new ProcessStartInfo();
+                        disableProtection.FileName = "cmd.exe";
+                        disableProtection.Arguments = "/c vssadmin delete shadows /all /quiet";
+                        disableProtection.WindowStyle = ProcessWindowStyle.Hidden;
+                        disableProtection.Verb = "runas"; // Request elevation
+
+                        using (Process process = Process.Start(disableProtection))
+                        {
+                            process.WaitForExit();
+
+                            if (process.ExitCode == 0)
+                            {
+                                MessageBox.Show("Deleted all restore points.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                LoadRestorePoints();
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Failed to delete restore points. Exit code: {process.ExitCode}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    catch (Win32Exception ex)
+                    {
+                        MessageBox.Show($"Failed to run cmd: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void RestorePoint_Text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
